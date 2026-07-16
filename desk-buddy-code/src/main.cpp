5dcdd1270f95a194f1f5710aba2066cd686b3ac8
Toan Do <74166553+Rubey4112@users.noreply.github.com>
2026-07-16 19:56:01 -0400

    tested displaying battery percentage

--- desk-buddy-code/src/main.cpp
+++ desk-buddy-code/src/main.cpp
@@ -1,18 +1,27 @@
-#include <Arduino.h>
+#include "M5Unified.h"
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  M5.Display.setTextDatum(middle_center);
+  M5.Display.setTextColor(TFT_BLACK);
+  M5.Display.setTextFont(&fonts::Orbitron_Light_24);
+  M5.Display.setTextSize(1);
+  M5.Display.setBrightness(5);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  M5.Display.clear(TFT_WHITE);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  bool isCharging = M5.Power.isCharging();
+  int vol_per = M5.Power.getBatteryLevel();
+
+  M5.Display.setCursor(0, 30);
+  M5.Display.printf("Charging: %s \n\n", isCharging ? "Yes" : "No");
+  M5.Display.setCursor(0, 60);
+  M5.Display.printf("BAT: %d%%", vol_per);
+  delay(5000);
 }
